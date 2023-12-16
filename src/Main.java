6b2b51c838a4623e178488b46fa56544181bbb28
emrekabakci <emrekabakci.2@icloud.com>
2023-12-16 11:04:42 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //New scanner created.
         Scanner inp=new Scanner(System.in);
-        System.out.println("Enter Number:");
 
+        //Data retrieved from user.
+        System.out.println("Enter Number:");
         int num = inp.nextInt();
 
+        //Inverted triangle printed with for loop.
         for (int i = num; i >= 0; i--) {
             for (int j = 0; j < num- i; j++) {
                 System.out.print(" ");
